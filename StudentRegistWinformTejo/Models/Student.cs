c843938cdd05d8bd3873da54848733eb7a7b3f73
christianjamestejo-ui <christianjames.tejo@dbtc-cebu.edu.ph>
2026-04-15 17:19:19 +0800

    Light Files for the folder

---
+++ StudentRegistWinformTejo/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRegistWinformTejo.Models
+{
+    public class Student
+    {
+        internal object Id;
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? MiddleInitial { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public string? Gender { get; set; }
+        public string? Address { get; set; }
+        public string? Course { get; set; }
+        public int? Year { get; set; }
+        public string? ImagePath { get; set; }
+        
+    }
+}
